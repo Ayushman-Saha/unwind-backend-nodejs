111edcb319b5de17407f16f4a153acc3a3d59bd5
Ayushman Saha <ayushman.2004.as@gmail.com>
2023-02-01 19:00:34 +0530

    get to post change

--- src/api/spotify-api/web/spotify-api.routes.ts
+++ src/api/spotify-api/web/spotify-api.routes.ts
@@ -4,8 +4,8 @@ import SpotifyApiController from "./spotify-api.controller";
 //Setting up routes for the spotify web Api 
 const spotifyWebApiRoute = Router()
 
-spotifyWebApiRoute.route('/getUserProfile').get(SpotifyApiController.getUserProfile)
-spotifyWebApiRoute.route('/getPlaylists').get(SpotifyApiController.getPlaylists)
+spotifyWebApiRoute.route('/getUserProfile').post(SpotifyApiController.getUserProfile)
+spotifyWebApiRoute.route('/getPlaylists').post(SpotifyApiController.getPlaylists)
 spotifyWebApiRoute.route('/getPlaylistTracks').post(SpotifyApiController.getPlaylistTracks)
 
 export default spotifyWebApiRoute
